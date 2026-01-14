4d09404cd1e91e3e5921e0b7ec0e9ef70b21c71d
Benediktus Satriya Mukti <benediktus_mukti@circles.asia>
2026-01-14 08:21:39 +0700

    Fix Astro output mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Benelabs.tech',
-  output: 'hybrid',
+  output: 'static',
   adapter: node({
     mode: 'standalone'
   }),
